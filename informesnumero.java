e0ca3c01b376b603ff421e247230f9327cc7d54c
Elek Almeida <kaykealmeida2017@gmail.com>
2026-03-22 08:33:14 -0300

    Add files via upload

---
+++ informesnumero.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class informesnumero {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+            System.out.print("Digite um número: ");
+        int numero = scanner.nextInt();
+
+            System.out.println("O número digitado é: " + numero);
+
+        scanner.close();
+    }
+    
+}
