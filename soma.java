deb2984fd50368a99fc17e5569c4558584b01d89
Elek Almeida <kaykealmeida2017@gmail.com>
2026-03-22 08:38:58 -0300

    Add files via upload

---
+++ soma.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class soma {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Digite o primeiro número: ");
+
+        int num1 = scanner.nextInt();
+        
+            System.out.print("Digite o segundo número: ");
+
+        int num2 = scanner.nextInt();
+        int soma = num1 + num2;
+
+            System.out.println("A soma dos números é: " + soma);
+
+        scanner.close();
+    }
+}
